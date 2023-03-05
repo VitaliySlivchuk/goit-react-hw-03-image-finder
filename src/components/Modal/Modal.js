89abc109df568e3bc39c86f50faf6b6bcd66fe8e
VitaliySlivchuk <41226137+VitaliySlivchuk@users.noreply.github.com>
2023-03-05 12:27:43 +0200

    орп

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import { createPortal } from 'react-dom';
+import PropTypes from 'prop-types';
 
 import { OverlayCss, ModalCss } from './Modal.styled';
 
 export const modalRoot = document.querySelector('#modal-root');
 
 export class Modal extends Component {
+  static propTypes = {
+    onClose: PropTypes.func.isRequired,
+    children: PropTypes.node.isRequired,
+  };
   componentDidMount() {
     window.addEventListener('keydown', this.handleKeyDown);
   }
